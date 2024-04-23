b8528ce87c2b6946ab8aa6391182366a3c17480e
Oscar El <opelm1@gmail.com>
2024-04-23 19:01:11 +1000

    Add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,23 @@ const cursive = Cursive({
 export const metadata = {
   title: "Your Next Web Design",
   description: "The next level of web design",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  icons: [
+    { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png" },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+    { rel: "manifest", url: "/site.webmanifest" },
+    { rel: "icon", url: "/favicon.ico" },
+  ],
 };
 
 export default function RootLayout({
